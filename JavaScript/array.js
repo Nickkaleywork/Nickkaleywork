74c0599b78758e10509cf73fe002141e6347d799
Nickkaleywork <165393218+Nickkaleywork@users.noreply.github.com>
2024-04-02 10:13:27 -0400

    First

---
+++ JavaScript/array.js
@@ -0,0 +1,183 @@
+let StationArray = [
+{
+line:"1",station:"1",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"2",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"3",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"4",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"5",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"6",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"7",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"8",
+state:"rgb(0 128 0)"
+},
+{
+line:"1",station:"9",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"1",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"2",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"3",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"4",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"5",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"6",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"7",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"8",
+state:"rgb(0 128 0)"
+},
+{
+line:"2",station:"9",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"1",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"2",
+state:"rgb(253 1 0)"
+},
+{
+line:"3",station:"3",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"4",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"5",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"6",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"7",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"8",
+state:"rgb(0 128 0)"
+},
+{
+line:"3",station:"9",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"1",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"2",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"3",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"4",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"5",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"6",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"7",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"8",
+state:"rgb(0 128 0)"
+},
+{
+line:"4",station:"9",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"1",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"2",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"3",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"4",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"5",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"6",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"7",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"8",
+state:"rgb(0 128 0)"
+},
+{
+line:"5",station:"9",
+state:"rgb(0 128 0)"
+}
+]
+
